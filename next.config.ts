986d00025ce9a10532aae638f9c21aa58da74f40
andrasbaki6 <andrasbaki6@gmail.com>
2025-10-19 16:59:59 +0000

    Update env config.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type {NextConfig} from 'next';
-import { initializeApp } from "firebase/app";
 
 const nextConfig: NextConfig = {
   
@@ -12,16 +11,5 @@ const nextConfig: NextConfig = {
 
   devIndicators: false,
 };
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDRKjQb54J4HfBbya7iwirs0TLxN25SFO8",
-  authDomain: "migreen-3xrnc.firebaseapp.com",
-  projectId: "migreen-3xrnc",
-  storageBucket: "migreen-3xrnc.firebasestorage.app",
-  messagingSenderId: "1068715304134",
-  appId: "1:1068715304134:web:d66e54d5a7ed53fd3a23c7"
-};
-
-const app = initializeApp(firebaseConfig);
 
 export default nextConfig;
