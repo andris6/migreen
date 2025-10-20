2081a99822f3511909c07efc85c87c1052404cea
andrasbaki6 <andrasbaki6@gmail.com>
2025-10-20 12:18:17 +0000

    Update error handling

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -15,31 +15,29 @@ interface Props {
 
 interface State {
   hasError: boolean;
-  showDialog: boolean;
 }
 
 class ErrorBoundary extends Component<Props, State> {
   public state: State = {
     hasError: false,
-    showDialog: false,
   };
 
-  public static getDerivedStateFromError(_: Error): Partial<State> {
+  public static getDerivedStateFromError(_: Error): State {
     return { hasError: true };
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     console.error('Uncaught error:', error, errorInfo);
-    setTimeout(() => {
-      this.setState({ showDialog: true });
-    }, 5000);
   }
 
   public render() {
     if (this.state.hasError) {
       return (
-        <Dialog open={this.state.showDialog}>
-          <DialogContent className="sm:max-w-[425px]" hideCloseButton={true}>
+        <Dialog open={this.state.hasError}>
+          <DialogContent 
+            className="sm:max-w-[425px] border-0 bg-secondary"
+            hideCloseButton={true}
+          >
             <DialogHeader>
               <DialogTitle className="flex flex-col items-center justify-center text-center">
                 <Logo className="mb-6 text-5xl justify-center" />
